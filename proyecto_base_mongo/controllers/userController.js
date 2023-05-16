baa3b8ebd80ea3b00f7cd3fd7ee5a5dfee8c99a7
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-05-16 16:22:04 -0300

    .

--- proyecto_base_mongo/controllers/userController.js
+++ proyecto_base_mongo/controllers/userController.js
@@ -7,40 +7,56 @@ async function index(req, res) {
   return res.render("users/showAll", { users });
 }
 
-// Display the specified resource.
-async function show(req, res) {}
+//async function show(req, res) {}
 
 // Show the form for creating a new resource
-async function create(req, res) {}
+async function create(req, res) {
+  return res.render("users/create");
+}
 
 // guarda el usuario creado-
 async function store(req, res) {
-  const newUser = {
+  const newUser = new User({
     firstname: req.body.firstname,
     lastname: req.body.lastname,
     age: req.body.age,
-  };
+  });
 
   await newUser.save();
 
   return res.redirect("/usuarios");
 }
 
-// Show the form for editing the specified resource.
-async function edit(req, res) {}
+async function edit(req, res) {
+  const user = await User.findById(req.params.id);
 
-// Update the specified resource in storage.
-async function update(req, res) {}
+  return res.render("users/edit", { user });
+}
 
-// Remove the specified resource from storage.
-async function destroy(req, res) {}
+async function update(req, res) {
+  const id = req.params.id;
 
-// Otros handlers...
-// ...
+  await User.updateOne(
+    { _id: id },
+    {
+      firstaname: req.body.firstname,
+      lastname: req.body.lastname,
+      age: req.body.age,
+    },
+  );
+
+  return res.redirect("/usuarios");
+}
+
+async function destroy(req, res) {
+  const id = req.params.id;
+  await User.deleteOne({ _id: id });
+
+  return res.render("users/showAll");
+}
 
 module.exports = {
   index,
-  show,
   create,
   store,
   edit,
--- proyecto_base_mongo/routes/userRoutes.js
+++ proyecto_base_mongo/routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require("../controllers/userController");
 
 router.get("/", userController.index);
 router.get("/crear", userController.create);
-router.get("/:id", userController.show);
+/*router.get("/:id", userController.show);*/
 router.post("/", userController.store);
 router.get("/editar/:id", userController.edit);
 router.patch("/:id", userController.update);
